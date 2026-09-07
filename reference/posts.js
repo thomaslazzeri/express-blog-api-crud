2ea1a3d1204ef0eece8d893dda5f9653b994d16a
thomas <thomaslazzeri3@gmail.com>
2026-09-07 19:38:21 +0200

    redone file posts add id, images and tags

--- reference/posts.js
+++ reference/posts.js
@@ -1,17 +1,37 @@
 export const posts = [
     {
-        id: 1, 
+        id: 1,
         title: 'Primo post',
-        content: 'Contenuto del primo post'
+        content: 'Contenuto del primo post',
+        image: "/images/post1.jpg",
+        tags: ["yellow", "text"],
     },
     {
-        id: 2 , 
+        id: 2,
         title: 'Secondo post',
-        content: 'Contenuto del secondo post'
+        content: 'Contenuto del secondo post',
+        image: "/images/post2.jpg",
+        tags: ["green", "text"],
     },
     {
-        id: 3 , 
+        id: 3,
         title: 'Terzo post',
-        content: 'Contenuto del terzo post'
+        content: 'Contenuto del terzo post',
+        image: "/images/post3.jpg",
+        tags: ["cian", "text"],
+    },
+    {
+        id: 4,
+        title: 'Quarto post',
+        content: 'Contenuto del quarto post',
+        image: "/images/post4.jpg",
+        tags: ["purple", "text"],
+    },
+    {
+        id: 5,
+        title: 'Quinto post',
+        content: 'Contenuto del quinto post',
+        image: "/images/post5.jpg",
+        tags: ["pink", "text"],
     }
 ];
